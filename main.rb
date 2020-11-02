7f6c1cf92640da7b93ba6679bbadf3b260c0be10
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-02 12:32:22 -0500

    starting strings explo

--- main.rb
+++ main.rb
@@ -7,15 +7,15 @@
 # variable assigment
 hello = "hello world"
 CONSTANT_VARIABLE = "k"
-puts hello
-puts CONSTANT_VARIABLE
+# puts hello
+# puts CONSTANT_VARIABLE
 
 # parallel variable assignment
 name, age, exact_age = "Sam", 25, 25.4
-puts name, age
+# puts name, age
 
 # inspecting class / type
-puts name.class, age.class, exact_age.class
+# puts name.class, age.class, exact_age.class
 
 # puts 2.methods OUTPUTS all methods for Integer sub-class of Numeric class
 
@@ -24,4 +24,12 @@ def add_two (number)
     number + 2
 end
 
-puts add_two(3)
+# puts add_two(3)
+
+# Defining Strings
+str_literal = 'it\'s nov 2 \nnewline'
+str_dbl_quotes = "it\'s nov 2 \nnewline"
+
+# puts str_literal, str_dbl_quotes
+
+puts 'Sam'.length
