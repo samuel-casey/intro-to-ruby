c52aa6de63726076375ead877fed08449cef74a2
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-02 12:06:59 -0500

    defined example variables and function

--- main.rb
+++ main.rb
@@ -1,1 +1,27 @@
-puts ("Hello world.")
+# README for lesson: https://git.generalassemb.ly/samuel-casey/ruby-intro-to-ruby
+=begin
+  REPLs
+  Joe's repl: https://repl.it/@jkeohan/intro-to-ruby-seir831#main.rb
+=end
+
+# variable assigment
+hello = "hello world"
+CONSTANT_VARIABLE = "k"
+puts hello
+puts CONSTANT_VARIABLE
+
+# parallel variable assignment
+name, age, exact_age = "Sam", 25, 25.4
+puts name, age
+
+# inspecting class / type
+puts name.class, age.class, exact_age.class
+
+# puts 2.methods OUTPUTS all methods for Integer sub-class of Numeric class
+
+# defining functions in Ruby
+def add_two (number)
+    number + 2
+end
+
+puts add_two(3)
